924f2d48c578368b864475e93a853355d6c3057d
Darius <justin50819just@gmail.com>
2024-09-08 22:42:06 +0800

    :commit0908

--- main.js
+++ main.js
@@ -1,3 +1,19 @@
 import './assets/scss/all.scss';
 
 console.log("Hello world!");
+// core version + navigation, pagination modules:
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+// init Swiper:
+const swiper = new Swiper('.swiper', {
+  // configure Swiper to use modules
+  modules: [Navigation, Pagination],
+  
+});
+
+
